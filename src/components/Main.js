30b7daa75e7f54150abb7bf5eaf0482d9fbf28e6
Dimas Aprillianto <dimasaprillianto6@gmail.com>
2023-05-24 18:16:12 +0700

    pertemuan94

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Hello from './Hello';
+
+/**
+ * Membuat Component Main
+ */
+function Main() {
+  return (
+    <div>
+      <main>
+        <Hello name="sinka" />
+        <Hello name="grachia" />
+        <Hello name="naomi" />
+        <Hello name="melody" />
+        <Hello name="zee" />
+      </main>
+    </div>
+  );
+}
+
+// export Component Main
+export default Main;
