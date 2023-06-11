e8777399ea1620970fa82bbb4fa352d5c98137b4
Dimas Aprillianto <72811633+dimaskun-a@users.noreply.github.com>
2023-06-11 22:15:58 +0700

    index.js

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,14 +1,8 @@
-/**
- * import CSS Module Hero
- * simpan di variable styles
- */
+
 import React from 'react';
 import styles from "./index.module.css";
 
-/**
- * Membuat Component Container.
- * Menggunakan Teknik Composition: children.
- */
+
 function Container({ children }) {
 return <div className={styles.container}>{children}</div>;
 }
